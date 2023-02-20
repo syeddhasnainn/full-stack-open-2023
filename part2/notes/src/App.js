5f92a6c303d78fa7a2373e506b0c852f45b610d4
Syed Hasnain <syedhasnainmurtaza@gmail.com>
2023-02-20 21:51:03 +0500

    changed default error message in useState

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
 
   useEffect(() => {
